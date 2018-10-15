1ae10fa77ba57dd9c4258ec6360059a75d957c1e
Alejandro <alejandro@Alejandros-MacBook-Pro.local>
2018-10-14 21:12:40 -0500

    adding LocalRime, LocalDate and LocalDateTime

---
+++ src/com/Interface/TestInterface.java
@@ -0,0 +1,13 @@
+package com.Interface;
+
+public interface TestInterface {
+	
+	int m1(int i);
+
+	public static void main(String[] args) {
+		
+		System.out.println("In Thread main :)");
+		
+	}//Close main
+	
+}//Close TestInterface
